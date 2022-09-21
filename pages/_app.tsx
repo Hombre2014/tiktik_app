a9ac7ccdb0568af8d658c8d21d9cafac5d329314
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-09-21 17:24:45 +0200

    Add and configure Tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+
+  return(
+    <div>
+      Navbar
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl-hover-overflow-auto'>
+          Sidebar
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh]'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default MyApp
+export default MyApp;
